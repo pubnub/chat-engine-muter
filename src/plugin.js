7e8ff34b0a4ce4f0b7c6136e5c95f4bc4d7c44f2
manuelfernando <manuelcamaron@hotmail.com>
2018-09-07 20:31:15 -0500

    resolve eslint issues

--- src/plugin.js
+++ src/plugin.js
@@ -21,99 +21,92 @@
  * // false
  */
 module.exports = (config = {}) => {
+    class extension {
+      /**
+       * Check if a {@link User} is muted within the {@link Chat}.
+       * @method muter"."isMuted
+       * @ceextends Chat
+       */
+      isMuted(user) {
+          let state = this.parent.meta;
 
-  class extension {
-
-    construct() {
-    }
-
-    /**
-     * Check if a {@link User} is muted within the {@link Chat}.
-     * @method muter"."isMuted
-     * @ceextends Chat
-     */
-    isMuted(user) {
-      let state = this.parent.meta;
-
-      if (state && state.muted && state.muted[config.me.uuid] && state.muted[config.me.uuid].indexOf(user.uuid) >= 0) {
-        return true;
-      } else {
-        return false;
-      }
-    }
-
-    /**
-     * Prevent all events emitted from this {@link User} from reaching {@link Chat}.
-     * @method muter"."mute
-     * @ceextends Chat
-     */
-    mute(user) {
-      let state = this.parent.meta || {};
-
-      if (!state.muted) {
-        state.muted = {};
+          if (state && state.muted && state.muted[config.me.uuid] && state.muted[config.me.uuid].indexOf(user.uuid) >= 0) {
+              return true;
+          } else {
+              return false;
+          }
       }
 
-      if (!state.muted[config.me.uuid]) {
-        state.muted[config.me.uuid] = [];
+      /**
+       * Prevent all events emitted from this {@link User} from reaching {@link Chat}.
+       * @method muter"."mute
+       * @ceextends Chat
+       */
+      mute(user) {
+          let state = this.parent.meta || {};
+
+          if (!state.muted) {
+              state.muted = {};
+          }
+
+          if (!state.muted[config.me.uuid]) {
+              state.muted[config.me.uuid] = [];
+          }
+
+          state.muted[config.me.uuid].push(user.uuid);
+
+          this.parent.update(state);
       }
 
-      state.muted[config.me.uuid].push(user.uuid);
+      /**
+       * Allow events from a {@link User} to be emitted again.
+       * @method muter"."unmute
+       * @ceextends Chat
+       */
+      unmute(user) {
+          let state = this.parent.meta;
 
-      this.parent.update(state);
-    }
+          if (state.muted && state.muted[config.me.uuid] && state.muted[config.me.uuid].indexOf(user.uuid) >= 0) {
+              var index = state.muted[config.me.uuid].indexOf(user.uuid);
 
-    /**
-     * Allow events from a {@link User} to be emitted again.
-     * @method muter"."unmute
-     * @ceextends Chat
-     */
-    unmute(user) {
-      let state = this.parent.meta;
+              state.muted[config.me.uuid].splice(index, 1);
 
-      if (state.muted && state.muted[config.me.uuid] && state.muted[config.me.uuid].indexOf(user.uuid) >= 0) {
-        var index = state.muted[config.me.uuid].indexOf(user.uuid);
-
-        state.muted[config.me.uuid].splice(index, 1);
-
-        this.parent.update(state);
+              this.parent.update(state);
+          }
       }
+    };
+
+    let muteFilter = (payload, next) => {
+        let isOwnEvent = false;
+
+        if (payload && payload.event && payload.event.indexOf('$.muter') > -1) {
+            isOwnEvent = true;
+        }
+
+        if(!isOwnEvent && payload && payload.sender && payload.chat && payload.chat.muter && payload.chat.muter.isMuted(payload.sender)) {
+
+            payload.chat.trigger('$.muter.eventRejected', {
+                originalEvent: payload.event,
+                sender: payload.sender.uuid,
+                chat: payload.chat.objectify()
+            });
+
+            next(true); // reject message and stop it from emitting
+
+        } else {
+            next(null, payload);
+        }
+    };
+
+    return {
+        namespace: 'muter',
+        extends: {
+          Chat: extension
+        },
+        middleware: {
+          on: {
+            '*': muteFilter
+          }
+        }
     }
-
-  };
-
-  let muteFilter = (payload, next) => {
-    let isOwnEvent = false;
-
-    if (payload && payload.event && payload.event.indexOf('$.muter') > -1) {
-      isOwnEvent = true;
-    }
-
-    if(!isOwnEvent && payload && payload.sender && payload.chat && payload.chat.muter && payload.chat.muter.isMuted(payload.sender)) {
-
-      payload.chat.trigger('$.muter.eventRejected', {
-        originalEvent: payload.event,
-        sender: payload.sender.uuid,
-        chat: payload.chat.objectify()
-      });
-
-      next(true); // reject message and stop it from emitting
-
-    } else {
-      next(null, payload);
-    }
-
-  };
-
-  return {
-    namespace: 'muter',
-    extends: {
-      Chat: extension
-    },
-    middleware: {
-      on: {
-        '*': muteFilter
-      }
-    }
-  }
 }
